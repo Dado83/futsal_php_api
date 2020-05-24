7c3acf1ee85dcaa3ac61b80adb9488df50e884cf
Dado83 <83deadpool@gmail.com>
2020-05-24 17:45:11 +0200

    Add newsletter

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -90,34 +90,150 @@ class Home extends BaseController
         echo view('fts.html');
     }
 
-    public function getLastResults($results)
+    public function admin()
     {
-        $data = $this->model->getLastResults($results);
-        return $this->response->setJSON($data);
+        $data['results'] = $this->model->getResults();
+        $data['matchPairs'] = $this->model->getMatchPairsNotPlayed();
+
+        echo view('header');
+        echo view('admin', $data);
+        echo view('footer');
+
     }
 
-    public function getNextFixture()
+    public function formIn($id)
     {
-        $data = $this->model->getNextFixture();
-        return $this->response->setJSON($data);
+
+        $data['title'] = 'Unos kola';
+        $data['game'] = $this->model->getGameByID($id);
+
+        echo view('header', $data);
+        echo view('gameInput', $data);
     }
 
-//????????????
-    public function setVisitor($role = 'NULL')
+    public function unosKola()
     {
-        $data = $this->model->setVisitor();
-        return $this->response->setJSON($data);
+
+        $request = service('request');
+
+        $id = $request->getVar('id');
+        $mDay = $request->getVar('mday');
+
+        $home = $request->getVar('home');
+        $away = $request->getVar('away');
+        $homeID = $request->getVar('homeID');
+        $awayID = $request->getVar('awayID');
+
+        $home10 = $request->getVar('home10');
+        $away10 = $request->getVar('away10');
+        $home9 = $request->getVar('home9');
+        $away9 = $request->getVar('away9');
+        $home8 = $request->getVar('home8');
+        $away8 = $request->getVar('away8');
+        $home7 = $request->getVar('home7');
+        $away7 = $request->getVar('away7');
+        $home6 = $request->getVar('home6');
+        $away6 = $request->getVar('away6');
+
+        $this->model->insertGame($mDay, $home, $homeID, $away, $awayID,
+            $home7, $away7,
+            $home8, $away8,
+            $home9, $away9,
+            $home10, $away10,
+            $home6, $away6);
+
+        $this->model->setPlayed($id, true);
+
+        /* $data['results'] = $this->model->getResults();
+        $data['matchPairs'] = $this->model->getMatchPairsNotPlayed(); */
+        return redirect()->to('admin');
     }
 
-    public function visitorListForCurrentYear()
+    public function brisanjeKola($id)
     {
-        $data = $this->model->visitorListForCurrentYear();
-        return $this->response->setJSON($data);
+
+        $data['matchPairs'] = $this->model->getMatchPairsNotPlayed();
+        $data['results'] = $this->model->getAllResults('results6');
+
+        $game = $this->model->getGameFromResults($id);
+        $game7 = $this->model->getGameBySel('results7', $game->home_teamid, $game->away_teamid);
+        $game10 = $this->model->getGameBySel('results10', $game->home_teamid, $game->away_teamid);
+
+        $matchPair = $this->model->getMatchPair($game->home_teamid, $game->away_teamid);
+
+        $this->model->setPlayed($matchPair->id, 'FALSE');
+
+        $this->model->deleteGame('results6', 'table6', $id);
+        $this->model->deleteGame('results8', 'table8', $id);
+        $this->model->deleteGame('results9', 'table9', $id);
+        if ($game7 != 'NULL') {
+            $this->model->deleteGame('results7', 'table7', $game7->id);
+        }
+        if ($game10 != 'NULL') {
+            $this->model->deleteGame('results10', 'table10', $game10->id);
+        }
+
+        return redirect()->to('admin');
+    }
+
+    public function newsLetter()
+    {
+        $data['title'] = 'Bilten';
+        $data['teams'] = $this->model->getTeams();
+        $data['table6'] = $this->model->getTable('table6');
+        $data['table7'] = $this->model->getTable('table7', false, 8);
+        $data['table8'] = $this->model->getTable('table8');
+        $data['table9'] = $this->model->getTable('table9');
+        $data['table10'] = $this->model->getTable('table10', false, 7, 1);
+        $data['results6'] = $this->model->getLastResults('results6')['results'];
+        $data['results7'] = $this->model->getLastResults('results7')['results'];
+        $data['results8'] = $this->model->getLastResults('results8')['results'];
+        $data['results9'] = $this->model->getLastResults('results9')['results'];
+        $data['results10'] = $this->model->getLastResults('results10')['results'];
+        $data['lastMday'] = $this->model->getLastResults('results6')['lastMday'];
+        $data['nextFixture'] = $this->model->getNextFixture();
+        $data['nextMday'] = $data['lastMday'] + 1;
+        $data['notPlaying'] = $this->model->getNotPlaying($data['lastMday'] + 1);
+        $data['notPlayingLastMday'] = $this->model->getNotPlaying($data['lastMday']);
+        $data['nextGameDate'] = $this->model->getNextGameDate($data['nextMday']);
+        $maxMday = $this->model->getMaxMday();
+        $data['isLeagueOver'] = $data['lastMday'] == $maxMday->mDay;
+
+        echo view('newsletter', $data);
+    }
+
+    public function metrics()
+    {
+        $data['title'] = 'Metrics';
+        $data['vis'] = $this->model->visitorListForCurrentYear();
+        echo view('metrics', $data);
+
+    }
+    public function getVisitorData()
+    {
+
+        $data['visAll'] = $this->model->getVisitors('all');
+        $data['visUni'] = $this->model->getVisitors('allUnique');
+        $data['visDesk'] = $this->model->getVisitors('desktop');
+        $data['visDeskUni'] = $this->model->getVisitors('desktopUnique');
+        $data['visMob'] = $this->model->getVisitors('mobile');
+        $data['visMobUni'] = $this->model->getVisitors('mobileUnique');
+        $data['visRob'] = $this->model->getVisitors('robot');
+        $data['visRobUni'] = $this->model->getVisitors('robotUnique');
+        $data['visitorList'] = $this->model->visitorListForCurrentYear();
+
+        echo json_encode($data);
     }
 
     public function test()
     {
-        echo 'test jebeni';
-    }
+        $data = $this->model->getGameByID(20);
 
+        $home = -1;
+        $away = 0;
+
+        if (5 > 2 && 0 == 0 || 1 == 2) {
+            echo 'test';
+        }
+    }
 }
