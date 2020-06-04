72a769182a70b0114a87943155f83ecb267708c1
Dado83 <83deadpool@gmail.com>
2020-06-04 22:13:22 +0200

    Add media queries to css

--- app/Views/header.php
+++ app/Views/header.php
@@ -7,7 +7,8 @@
     <meta name="keywords" content="Fair Play, Liga Budućih Šampiona, LBŠ, Liga">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#fcc914">
-    <link rel="stylesheet" href="/public/css/styles.css?v=0.9">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/styles.css?v=1.0">
     <link rel="shortcut icon" href="/public/images/fp.ico" type="image/x-icon">
     <script src="/public/js/scripts.js?v=0.1" defer></script>
     <script src="/public/js/Chart.bundle.min.js" defer></script>
