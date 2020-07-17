846cb897db7753b6a276b800a162dd3a1cd4d326
Dado83 <83deadpool@gmail.com>
2020-07-17 19:30:51 +0200

    Style admin&metrics pages

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -12,11 +12,11 @@
     </form>
 </div>
 <div id="adminCards">
-    <div class="adminCards"><a href="/Home/newsLetter"><img src="/images/icons/newsletter.svg" style="width: 100%" />Bilten</a>
+    <div class="adminCards"><a href="/Home/newsLetter"><img class="club-logo__fixed" src="/public/images/icons/newsletter.svg" style="width: 100%" />Bilten</a>
     </div>
-    <div class="adminCards"><a href="/Home/metrics"><img src="/images/icons/charts.svg" style="width: 100%" />Metrics</a>
+    <div class="adminCards"><a href="/Home/metrics"><img class="club-logo__fixed" src="/public/images/icons/charts.svg" style="width: 100%" />Metrics</a>
     </div>
-    <div id="passwordChange" class="adminCards"><img src="/images/icons/pass.svg" style="width: 100%" />Password
+    <div id="passwordChange" class="adminCards"><img class="club-logo__fixed" src="/public/images/icons/pass.svg" style="width: 100%" />Password
     </div>
 </div>
 <table class="admin">
@@ -32,7 +32,9 @@
         <td class="oddResRow"><?=$row->m_day?></td>
         <td><?=$row->home_name?></td>
         <td><?=$row->away_name?></td>
-        <td><a class='button' href='/Home/deleteGame/<?=$row->id?>' onclick="return confirm('Brišem zadnje kolo?')">Briši</a></td>
+        <td>
+            <a class='button' href='/Home/deleteGame/<?=$row->id?>' onclick="return confirm('Brišem zadnje kolo?')">Briši</a>
+        </td>
     </tr>
     <?php endforeach?>
 </table>
@@ -53,3 +55,25 @@
     </tr>
     <?php endforeach?>
 </table>
+<script>
+document.querySelector('#passwordChange').addEventListener('mouseup', passwordChange)
+
+let modalForm = document.querySelector('#mForm')
+
+function passwordChange() {
+    modalForm.style.display = 'block'
+}
+
+window.addEventListener("click", function(e) {
+    if (e.target.id == modalForm.id) {
+        modalForm.style.display = 'none'
+    }
+})
+
+let oddRow = document.querySelectorAll('.oddResRow')
+for (el of oddRow) {
+    if (el.innerText % 2 != 0) {
+        el.style.background = 'gray'
+    }
+}
+</script>
--- app/Views/metrics.php
+++ app/Views/metrics.php
@@ -1,9 +1,9 @@
 <p>Dashboard:</p>
-<div class="chartContainer">
+<!-- <div class="chartContainer">
     <div><canvas id="visitorPercentage"></canvas></div>
     <div><canvas id="visitorPie"></canvas></div>
     <div><canvas id="visitorTimeline"></canvas></div>
-</div>
+</div> -->
 <p>Requests za <?=date('Y', time())?>. godinu</p>
 <div>
     <?php
@@ -15,16 +15,15 @@ foreach ($keys as $k):
         <table class="visitorTable">
             <thead>
                 <tr>
-                    <th>role</th>
-                    <th>return vis</th>
-                    <th>ip</th>
+                    <th class="hidden-metrics">return vis</th>
+                    <th class="hidden-metrics">ip</th>
                     <th>device</th>
-                    <th>browser</th>
-                    <th>browser ver</th>
-                    <th>mobile</th>
-                    <th>platform</th>
-                    <th>referral</th>
-                    <th>agent</th>
+                    <th class="hidden-metrics">browser</th>
+                    <th class="hidden-metrics">browser ver</th>
+                    <th class="hidden-metrics">mobile</th>
+                    <th class="hidden-metrics">platform</th>
+                    <th class="hidden-metrics">referral</th>
+                    <th class="hidden-metrics">agent</th>
                     <th>page</th>
                     <th>date</th>
                     <th>time</th>
@@ -35,16 +34,15 @@ foreach ($keys as $k):
     for ($i = 0; $i < count($vis[$k]); $i++):
     ?>
                 <tr>
-                    <td><?=$vis[$k][$i]->role?></td>
-                    <td><?=$vis[$k][$i]->returnVisitor?></td>
-                    <td><?=$vis[$k][$i]->ip?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->returnVisitor?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->ip?></td>
                     <td><?=$vis[$k][$i]->device?></td>
-                    <td><?=$vis[$k][$i]->browser?></td>
-                    <td><?=$vis[$k][$i]->browserVersion?></td>
-                    <td><?=$vis[$k][$i]->mobile?></td>
-                    <td><?=$vis[$k][$i]->platform?></td>
-                    <td><?=$vis[$k][$i]->referral?></td>
-                    <td><?=$vis[$k][$i]->agent?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->browser?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->browserVersion?></td>
+                    <td class="hidden-metrics" vis[$k][$i]->mobile?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->platform?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->referral?></td>
+                    <td class="hidden-metrics"><?=$vis[$k][$i]->agent?></td>
                     <td><?=$vis[$k][$i]->page?></td>
                     <td><?=$vis[$k][$i]->date?></td>
                     <td><?=$vis[$k][$i]->time?></td>
@@ -55,3 +53,11 @@ foreach ($keys as $k):
     </details>
     <?php endforeach?>
 </div>
+<script>
+let columns = document.querySelectorAll('.hidden-metrics')
+if (window.innerWidth < 900) {
+    for (col of columns) {
+        col.style.display = 'none'
+    }
+}
+</script>
