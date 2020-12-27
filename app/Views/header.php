f12cc15d3d3a63d8faeed4f9c85007c4c6c18e8d
Dado83 <83deadpool@gmail.com>
2020-12-27 18:44:40 +0100

    Fix standings width

--- app/Views/header.php
+++ app/Views/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#fcc914">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/public/css/styles.css?v=2.8">
+    <link rel="stylesheet" href="/public/css/styles.css?v=3.1">
     <link rel="shortcut icon" href="/public/images/fp.ico" type="image/x-icon">
     <script src="/public/js/scripts.js?v=0.1" defer></script>
     <script src="/public/js/Chart.bundle.min.js" defer></script>
@@ -37,7 +37,7 @@ if (session()->role == 'admin'): ?>
             </div>
             <ul class="nav-bar">
                 <li><a href="/rezultati">rezultati</a></li>
-                <li><a href="/tabela">tabela</a></li>
+                <li><a href="/tabela">tabele</a></li>
                 <li><a href="/raspored">raspored</a></li>
                 <li><a href="/zavrsnica">završnica</a></li>
                 <li><a href="/ucesnici">učesnici</a></li>
