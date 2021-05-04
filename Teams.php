c60a3a4c17ff17d3ddd08fd24b72357e8d559201
Dado83 <83deadpool@gmail.com>
2021-05-04 20:58:18 +0200

    Delete files

--- Teams.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header("Content-Type:application/json");
-require 'DB.php';
-
-$db = new DB('localhost', 'root', '', 'futsal');
-$db->DBconnect();
-$id = $_GET['id'];
-$db->select($id);
