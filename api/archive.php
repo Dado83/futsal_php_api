3611a7d5bc6285932a86bfeca7c987ff8d75a324
Dado83 <83deadpool@gmail.com>
2021-06-27 18:05:18 +0200

    Add all time table query

--- api/archive.php
+++ api/archive.php
@@ -14,4 +14,19 @@ function getArchive($year, $db)
     echo json_encode($arr);
 }
 
-getArchive($year, $db);
+function getAlltimeTable($db)
+{
+    $sql = "SELECT * FROM all_time_table ORDER BY points DESC";
+    $result = $db->query($sql);
+    $arr = [];
+    while ($row = $result->fetch_object()) {
+        $arr[] = $row;
+    }
+    echo json_encode($arr);
+}
+
+if ($year == 'alltime') {
+    getAlltimeTable($db);
+} else {
+    getArchive($year, $db);
+}
