bc8df397b8c9225590d0cddadb9cff16ce566fc0
Dado83 <83deadpool@gmail.com>
2021-07-04 18:11:39 +0200

    Get users from db

--- api/Users.php
+++ api/Users.php
@@ -1,20 +1,35 @@
 <?php
 require 'db.php';
 
-$user = $_GET['user'];
-$pass = $_GET['pass'];
+$user = $_GET['user'] ?? '';
+$pass = $_GET['pass'] ?? '';
 
 function checkLogin($user, $pass, $db)
 {
     $sql = "SELECT * FROM users WHERE role = '$user'";
     $query = $db->query($sql);
-    $dbPass = $query->fetch_object()->password;
+    $dbPass = $query->fetch_object();
 
-    if (!password_verify($pass, $dbPass)) {
-        echo json_encode('njet');
-    } else {
-        echo json_encode('admin');
+    if (isset($dbPass)) {
+        if (!password_verify($pass, $dbPass->password)) {
+            echo json_encode('not-admin');
+        } else {
+            echo json_encode('admin');
+
+        }
     }
 }
 
-checkLogin($user, $pass, $db);
+function getUsers($db)
+{
+    $sql = "SELECT * FROM users";
+    $query = $db->query($sql);
+    $result = $query->fetch_object();
+    echo json_encode($result);
+}
+
+if ($user != '') {
+    checkLogin($user, $pass, $db);
+} else {
+    getUsers($db);
+}
