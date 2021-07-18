27ddc5f7271f4be88b0881b9362587e294b48d83
Dado83 <83deadpool@gmail.com>
2021-07-18 15:25:26 +0200

    Get not yet played fixtures by mday

--- api/Fixtures.php
+++ api/Fixtures.php
@@ -2,6 +2,7 @@
 require 'db.php';
 
 $mDay = $_GET['mday'];
+$notPlayed = $_GET['notPlayed'] ?? null;
 
 function getFixtures($db, $mDay, $id = 12)
 {
@@ -19,14 +20,14 @@ function getFixtures($db, $mDay, $id = 12)
     echo json_encode($arr);
 }
 
-function getFixturesNotPlayed($db, $id = 12)
+function getFixturesNotPlayed($db, $mDay, $id = 12)
 {
     $sql = "SELECT matchpairs.id, matchpairs.m_day, matchpairs.home_team, matchpairs.away_team, matchpairs.game_date,
-        home.team_name AS home_team, away.team_name AS away_team
+        home.team_name AS home, away.team_name AS away
         FROM matchpairs
         JOIN teams AS home ON matchpairs.home_team = home.id
         JOIN teams AS away ON matchpairs.away_team = away.id
-        WHERE matchpairs.is_played = FALSE AND NOT (matchpairs.home_team = $id XOR matchpairs.away_team = $id)";
+        WHERE m_day = $mDay AND matchpairs.is_played = FALSE AND NOT (matchpairs.home_team = $id XOR matchpairs.away_team = $id)";
     $result = $db->query($sql);
     $arr = [];
     while ($row = $result->fetch_object()) {
@@ -35,9 +36,8 @@ function getFixturesNotPlayed($db, $id = 12)
     echo json_encode($arr);
 }
 
-if ($mDay !== 'notPlayed') {
+if (isset($_GET['notPlayed'])) {
+    getFixturesNotPlayed($db, $mDay);
+} else {
     getFixtures($db, $mDay);
-} elseif ($mDay === 'notPlayed') {
-    getFixturesNotPlayed($db);
 }
-//getFixturesNotPlayed($db);
