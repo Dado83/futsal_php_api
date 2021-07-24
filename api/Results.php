a6278e2e790a99997271d56151b4dbe85bdf75fb
Dado83 <83deadpool@gmail.com>
2021-07-24 13:41:08 +0200

    Add prev next mday

--- api/Results.php
+++ api/Results.php
@@ -4,6 +4,8 @@ require 'db.php';
 $mDay = $_GET['mday'] ?? '';
 $clubID = $_GET['clubid'] ?? '';
 $maxMday = $_GET['maxmday'] ?? false;
+$prevRes = $_GET['prevres'] ?? '';
+$nextFix = $_GET['nextfix'] ?? '';
 
 function getResultsByClub($db, $id)
 {
@@ -34,6 +36,22 @@ function getMaxMday($db)
     echo json_encode($result->fetch_object()->mDay);
 }
 
+function getPrevResultsMday($db)
+{
+    $sql1 = "SELECT MAX(m_day) as mDay FROM results";
+    $res1 = $db->query($sql1);
+    $maxMday = $res1->fetch_object()->mDay;
+    echo json_encode($maxMday);
+}
+
+function getNextFixMday($db)
+{
+    $sql1 = "SELECT MAX(m_day) as mDay FROM results";
+    $res1 = $db->query($sql1);
+    $maxMday = $res1->fetch_object()->mDay + 1;
+    echo json_encode($maxMday);
+}
+
 if ($maxMday != false) {
     getMaxMday($db);
 }
@@ -42,4 +60,12 @@ if ($clubID != '') {
 }
 if ($mDay != '') {
     getResultsByMday($db, $mDay);
+}
+
+if ($prevRes == 'prevres') {
+    getPrevResultsMday($db);
+}
+
+if ($nextFix == 'nextfix') {
+    getNextFixMday($db);
 }
